e2d969b96d3e0e54df34c9527afe6448ddac1928
Leslie <dejagerleslie@gmail.com>
2021-09-29 18:44:24 +0200

    Remove old version

--- MediaView/Angular7/projects/media-view-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of media-view-lib
- */
-
-export * from './lib/media-view-lib.service';
-export * from './lib/media-view-lib.component';
-export * from './lib/media-view-lib.module';
